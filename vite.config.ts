140e266e9ed4a03644a106e0287dfff2c5982f10
priyaahm <130679111+priyaahm@users.noreply.github.com>
2025-04-14 21:50:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Paradise-Hotel-App',
+  base: '/',  // Change from '/Paradise-Hotel-App' to '/'
   server: {
     port: 5173,
     strictPort: true,
@@ -14,4 +14,3 @@ export default defineConfig({
     sourcemap: true,
   },
 });
-
