740b37aa2304c820a4715c60cc19ecd12df406d9
priyaahm <marrapusripriya3@gmail.com>
2025-04-28 22:21:44 +0530

    Added.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -204,6 +204,12 @@ const Home = () => {
             <div>
               <h3 className="text-xl font-semibold mb-4">Quick Links</h3>
               <div className="space-y-2">
+                <Link
+                  to="/about"
+                  className="block text-blue-600 hover:text-blue-700"
+                >
+                  About Us
+                </Link>
                 <a
                   href="https://www.zomato.com/puducherry"
                   className="block text-blue-600 hover:text-blue-700"
